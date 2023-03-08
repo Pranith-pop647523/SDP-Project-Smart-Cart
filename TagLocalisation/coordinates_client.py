44e1c5cefadd7b543d677c11dfeaf6de29122546
Anthos Makris <s2036401@ed.ac.uk>
2023-03-08 13:51:40 +0000

    fixed code, now working

--- TagLocalisation/coordinates_client.py
+++ TagLocalisation/coordinates_client.py
@@ -7,13 +7,14 @@ import datetime
 import json
 from socket import *
 
-#socket parameters
-IP = "129.215.2.45" #change to IP of DICE machine server
-SERVER_PORT = 40000
-BUFLEN = 256
+#TCP parameters
+IP = '129.215.2.45' #IP address of DICE machine / server
+SERVER_PORT = 50000
+BUFLEN = 1024
 
-#initialise socket
-dataSocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+#create TCP socket
+dataSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)
+dataSocket.connect((IP,SERVER_PORT))
 
 #connect to serial port of UWB sensor
 DWM = serial.Serial(port="/dev/ttyACM0", baudrate=115200)
@@ -24,29 +25,28 @@ DWM.write("\r\r".encode())
 print("Encode")
 time.sleep(1)
 
-#send command lec to start receiving measurements
+#send command 'lec' to start receiving sensor measurements
 DWM.write("lec\r".encode())
 print("Encode")
 
 time.sleep(1)
+
+#loop reads serial data and sends them to TCP socket
 try:
     while True:
         #attempt to read sensor reading.
         data = DWM.readline().decode()
         if(data):
-            #print(data)
-            #print(type(data))
             if ("DIST" in data and "AN0" in data and "AN1" in data and "AN2" in data):
                 data = data.replace("\r\n", "")
                 data = data.split(",")
+
                 if("DIST" in data):
                     anchor_Number = int(data[data.index("DIST")+1])
                     for i in range(anchor_Number):
-                        pos_AN = {"id": data[data.index("AN"+str(i))+1], "x": data[data.index("AN"+str(i))+2], "y": data[data.index(
-                            "AN"+str(i))+3], "dist": data[data.index("AN"+str(i))+5]}
+                        pos_AN = {"id": data[data.index("AN"+str(i))+1], "x": data[data.index("AN"+str(i))+2], "y": data[data.index("AN"+str(i))+3], "dist": data[data.index("AN"+str(i))+5]}
                         pos_AN = json.dumps(pos_AN)
-                        #print(pos_AN)
-                        #r.set('AN'+str(i), pos_AN)
+                #get x and y coordinates from data 
                 if("POS" in data):
                     pos = {"x": data[data.index("POS")+1],
                            "y": data[data.index("POS")+2]}
@@ -54,12 +54,14 @@ try:
                     print(pos)
                     #send coordinates to server
                     toSend = pos
-                    dataSocket.sendto(toSend.encode(), (IP, SERVER_PORT))
-                    #received = dataSocket.recvfrom(BUFLEN)
-    #DWM.write("\r".encode())
-    #DWM.close()
-    #dataSocket.close()
+                    dataSocket.send(toSend.encode())
+                    #wait for reply
+                    recved = dataSocket.recv(BUFLEN)
+                    if not recved:
+                        break
 
+                    print(recved.decode())
+                    
 except KeyboardInterrupt:
     DWM.write("\r".encode())
     DWM.close()
