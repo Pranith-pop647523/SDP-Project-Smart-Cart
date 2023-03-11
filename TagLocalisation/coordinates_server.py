da8f558bcfe34839441e4e551fb2729c56b19534
Anthos Makris <s2036401@ed.ac.uk>
2023-03-11 14:26:20 +0000

    Update coordinates_server.py

--- TagLocalisation/coordinates_server.py
+++ TagLocalisation/coordinates_server.py
@@ -1,66 +1,57 @@
 from socket import *
-import sys
 
 IP = '0.0.0.0'
-TAG_PORT = 5000
-UI_PORT = 10000
-TURTLEBOT_IP = ''
-TURTLEBOT_PORT = 15000
+
+TAG_PORT = 50000
+UI_PORT = 50001
 BUFLEN = 512
-TRACK = 2
-STOP = 1
-PARK = 3
-mode = TRACK
+STOP = '1'
+FOLLOW = '2'
+PARK = '3'
+mode = FOLLOW
 
-#----Use the following code if you want to input these arguments from the console
-#TAG_PORT = sys.argv[1]
-#UI_PORT = sys.argv[2]
-#TURTLEBOT_IP = sys.argv[3]
-#TURTLEBOT_PORT = sys.argv[4]
+tagListenSocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP)
+tagListenSocket.bind((IP,TAG_PORT))
+tagListenSocket.listen()
+print(f'The sever is start, waiting for tag to connect at port {TAG_PORT}')
 
-# The socket to receive coordinates from the tag
-tagSocket = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
-# The socket to receive commands from the UI
-UISocket = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
-UISocket.settimeout(5)
-# The socket to send information to the turtlebot
-tbSocket = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP)
-# The 
-
-tagSocket.bind((IP,TAG_PORT))
-
-print(f'The sever is start, waiting to connect at port {TAG_PORT}')
+uiListenSocket = socket(AF_INET,SOCK_STREAM, IPPROTO_TCP)
+uiListenSocket.bind((IP,UI_PORT))
+uiListenSocket.listen()
+print(f'The sever is start, waiting for UI to connect at port {UI_PORT}')
 
 
+
+tagDataSocket, addr = tagListenSocket.accept()
+print('Accept a tag client to connect:', addr)
+
+uiDataSocket, addr = uiListenSocket.accept()
+uiDataSocket.settimeout(5)
+print('Accept a UI client to connect:', addr)
 while True:
-    #-----Receive data from one raspberry pi (The coordinates)-----
-    received, addr = tagSocket.recvfrom(BUFLEN)
-    if not received:
-        break
-    coordinates = received.decode()
-    print(f'Recieved the coordinates: {coordinates}')
-    #-----Receive data from the UI raspberry pi(Commands)----
     try:
-        received2, addr = UISocket.recvfrom(BUFLEN)
-        mode = received2.decode()
-    except(timeout):
+        command = uiDataSocket.recv(BUFLEN)
+        mode = command.decode()
+    except timeout:
         pass
 
-    # Send the coordinates to the turtle bot under the tracking mode
-    if(mode == TRACK):
-        #tbSocket.sendto(coordinates,(TURTLEBOT_IP,TURTLEBOT_PORT))
-        print(coordinates)
+    if mode == FOLLOW:
+        receivedTag = tagDataSocket.recv(BUFLEN)
+    
+    
+        if not receivedTag:
+            break
+    
+        info = receivedTag.decode()
+        print(f'Recieved the information: {info}')
+
+        tagDataSocket.send(f'The server received the information {info}'.encode())
+
+    elif mode == STOP:
+        print('Stop command')
 
     
-    # Tell the turtlebot to stop under the STOP mode
-    #elif(mode == STOP):
-        tbSocket.sendto()
-        
-    # Tell the turtlebot to go to the parking point
-    #elif(mode == PARK):
-
-
-    
-tagSocket.close()
-
-
+tagDataSocket.close()
+tagListenSocket.close()
+uiDataSocket.close()
+uiListenSocket.close()
